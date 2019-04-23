b057b9a919502f16d1b10575e65dad4e058ce61c
hung van tran <hung@623.local>
2019-04-23 11:27:49 +0200

    settings edited

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,14 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'www.waltblackney.me' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:hung623/hung623.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
